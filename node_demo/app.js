aa15896f95878a8de576fe2f0b7e20819e8d4962
Nick Steger <nsteger@iastate.edu>
2018-01-28 22:17:08 -0600

    Setup socket.io -- still needs work

--- node_demo/app.js
+++ node_demo/app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var app = express();
+
 var index = require('./routes/index');
 var about = require('./routes/about');
 var chat = require('./routes/chat');
@@ -13,15 +15,11 @@ var signOut = require('./routes/signOut');
 var register = require('./routes/register');
 var editAccount = require('./routes/editAccount');
 
-
-var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
