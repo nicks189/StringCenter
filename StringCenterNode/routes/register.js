029c2a239a7a55ccf0bf1be58196378ff92b9066
Nick Steger <nsteger@iastate.edu>
2018-01-30 23:46:40 -0600

    Update registration route and page

--- StringCenterNode/routes/register.js
+++ StringCenterNode/routes/register.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
     res.render('register', { title: 'Register', nav: 'register' });
 });
 
+router.post('/', function(req, res, next) {
+
+});
+
 module.exports = router;
