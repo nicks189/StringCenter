8974cf8926e14170b883d032458d7335bd8ff59f
Nick Steger <nsteger@iastate.edu>
2018-01-30 23:45:05 -0600

    Add mongoose, body-parser and passport to app

--- StringCenterNode/app.js
+++ StringCenterNode/app.js
@@ -4,6 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var expressSession = require('express-session');
+var db = require('db');
+var mongoose = require('mongoose');
+
+mongoose.connect(db.url);
 
 var app = express();
 
@@ -17,6 +23,10 @@ var editAccount = require('./routes/editAccount');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+// body parser setup
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
