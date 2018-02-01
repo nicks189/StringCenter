15a826511c501f432b4356b371987254ca6d1ff4
Nick Steger <nsteger@iastate.edu>
2018-02-01 15:03:31 -0600

    Now passing passport to routers

--- StringCenterNode/routes/register.js
+++ StringCenterNode/routes/register.js
@@ -1,38 +1,41 @@
 var express = require('express');
 var User = require('../models/user');
-var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.render('register', { title: 'Register', nav: 'register' });
-});
+module.exports = function(passport) {
+    var router = express.Router();
 
-router.post('/', function(req, res, next) {
-    if (req.body.username &&
-        req.body.password &&
-        req.body.confirmPassword) {
+    /* GET registration page. */
+    router.get('/', function(req, res, next) {
+        res.render('register', { title: 'Register', nav: 'register' });
+    });
 
-        // Add validation here
+    router.post('/', function(req, res, next) {
+        if (req.body.username &&
+            req.body.password &&
+            req.body.confirmPassword) {
+
+            // Add validation here
 
 
-        var newUser = {
-            username: req.body.username,
-            password: req.body.password,
-            confirmPassword: req.body.confirmPassword,
-            firstName: req.body.firstName,
-            lastName: req.body.lastName
-        }
-
-        User.create(newUser, function (error, user) {
-            if (error) {
-                return res.redirect('/register');
-            } else {
-                return res.redirect('/');
+            var newUser = {
+                username: req.body.username,
+                password: req.body.password,
+                confirmPassword: req.body.confirmPassword,
+                firstName: req.body.firstName,
+                lastName: req.body.lastName
             }
-        });
-    } else {
-        return next(new Error('All fields required').status(401));
-    }
-});
 
-module.exports = router;
+            User.create(newUser, function (error, user) {
+                if (error) {
+                    return res.redirect('/register');
+                } else {
+                    return res.redirect('/');
+                }
+            });
+        } else {
+            return next(new Error('All fields required').status(401));
+        }
+    });
+
+    return router;
+};
