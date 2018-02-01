716d6b61157688a0cec298289bfb04a25d306c6d
Nick Steger <nsteger@iastate.edu>
2018-02-01 15:02:43 -0600

    Using passport authentication now

--- StringCenterNode/routes/signIn.js
+++ StringCenterNode/routes/signIn.js
@@ -1,34 +1,23 @@
 var express = require('express');
 var User = require('../models/user');
-var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.render('sign-in', { title: 'Sign in', nav: 'sign-in' });
-});
+module.exports = function(passport) {
+    var router = express.Router();
 
-router.post('/', function(req, res, next) {
-    if (req.body.username &&
-        req.body.password) {
+    /* GET sign in page. */
+    router.get('/', function(req, res, next) {
+        res.render('sign-in', { title: 'Sign in', nav: 'sign-in' });
+    });
 
-        // Add validation here
+    /*
+     * Passport middleware handles authentication as specified in custom
+     * custom userAuth signIn strategy
+     */
+    router.post('/', passport.authenticate('signIn', {
+        failureRedirect: '/sign-in',
+        successRedirect: '/',
+        failureFlash: true
+    }));
 
-
-        var newUser = {
-            username: req.body.username,
-            password: req.body.password,
-        }
-
-        User.authenticate(newUser, function (error, user) {
-            if (error) {
-                return next(error);
-            } else {
-                return res.redirect('/');
-            }
-        });
-    } else {
-        return next(new Error('All fields required').status(401));
-    }
-});
-
-module.exports = router;
+    return router;
+};
