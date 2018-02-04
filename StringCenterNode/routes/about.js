abb1a8d670f223a57e9c5b8898d4d6be0c43af9c
Nick Steger <nsteger@iastate.edu>
2018-02-03 23:54:41 -0600

    require authentication

--- StringCenterNode/routes/about.js
+++ StringCenterNode/routes/about.js
@@ -1,10 +1,11 @@
 var express = require('express');
+var user = require('../models/user');
 
 module.exports = function(passport) {
     var router = express.Router();
 
     /* GET users listing. */
-    router.get('/', function (req, res, next) {
+    router.get('/', user.isAuthenticated, function(req, res, next) {
         res.render('about', {title: 'About', nav: 'about'});
     });
 
--- StringCenterNode/routes/editAccount.js
+++ StringCenterNode/routes/editAccount.js
@@ -1,10 +1,11 @@
 var express = require('express');
+var User = require('../models/user');
 
 module.exports = function(passport) {
     var router = express.Router();
 
     /* GET users listing. */
-    router.get('/', function(req, res, next) {
+    router.get('/', User.isAuthenticated, function(req, res, next) {
         res.render('edit-account', { title: 'Edit Account', nav: 'edit-account' });
     });
 
