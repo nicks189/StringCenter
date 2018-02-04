b942ce67e4cfcd3f6aef454553a8dac0a8a3de4a
Nick Steger <nsteger@iastate.edu>
2018-02-03 23:52:34 -0600

    Add registration authentication with passport

--- StringCenterNode/passport/userAuth.js
+++ StringCenterNode/passport/userAuth.js
@@ -1,5 +1,4 @@
 var LocalStrategy = require('passport-local').Strategy;
-var bcrypt = require('bcrypt-nodejs');
 var User = require('../models/user');
 
 
@@ -15,20 +14,52 @@ module.exports = function(passport) {
         });
     });
 
-    // Need to set strategy as passReqToCallback so we can access req object
+    /*
+     * Local authentication strategy for the user authentication. Can update this
+     * for REST api. Need to set strategy as passReqToCallback so we can access req
+     * object for flash message.
+     */
     passport.use('signIn', new LocalStrategy({ passReqToCallback : true },
         function(req, username, password, callback) {
+            // See if matching username exists in database
             User.findOne({ 'username' :  username}, function(error, user) {
                 if (error) {
                     return callback(error);
                 } else {
-                    if (user && User.comparePasswords(user.password, password)) {
+                    if (user && User.comparePasswords(password, user.password)) {
                         // Username and password match
                         return callback(null, user);
                     } else {
+                        // Either username or password were wrong. This can be changed.
                         return callback(null, false, req.flash('errorMessage', 'Invalid username or password'));
                     }
                 }
+            });
+        })
+    );
+
+    passport.use('register', new LocalStrategy({ passReqToCallback : true },
+        function(req, username, password, callback) {
+            User.findOne({ 'username' :  username}, function(error, user) {
+                if (error) {
+                    return callback(error);
+                } else if (user) {
+                    return callback(null, false, req.flash('errorMessage', 'Username is already taken'));
+                } else if (!(User.passwordConfirm(password, req.body.confirmPassword))) {
+                    return callback(null, false, req.flash('errorMessage', 'Passwords don\'t match'));
+                }
+                var newUser = new User();
+                newUser.username = username;
+                newUser.password = password;
+                newUser.firstName = req.body.firstName;
+                newUser.lastName = req.body.lastName;
+
+                newUser.save(function(error) {
+                    if (error) {
+                        return callback(error);
+                    }
+                    return callback(null, newUser);
+                });
             });
         })
     );
