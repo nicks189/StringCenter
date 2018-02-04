54bfcf69dda14521ee33ffb3d32288a5104c3d3a
Nick Steger <nsteger@iastate.edu>
2018-02-03 23:57:00 -0600

    Add password hashPassword(), comparePasswords, isAuthenticated

--- StringCenterNode/models/user.js
+++ StringCenterNode/models/user.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
 
 var UserSchema = mongoose.Schema({
     username: {
@@ -20,17 +21,48 @@ var UserSchema = mongoose.Schema({
         required: true,
         trim: true
     },
-    confirmPassword: {
-        type: String,
-        required: true,
-        trim: true
-    }
 });
 
-UserSchema.statics.comparePasswords = function(matchedPassword, password) {
-    // return bcrypt.compareSync(matchedPassword, password);
-    return matchedPassword === password;
+UserSchema.statics.comparePasswords = function(password, matchedPassword) {
+    // Should change to async
+    return bcrypt.compareSync(password, matchedPassword);
 };
+
+UserSchema.statics.hashPassword = function(password, callback) {
+    bcrypt.genSalt(10, function(error, salt) {
+        if (error) {
+            return callback(error);
+        }
+        bcrypt.hash(password, salt, null, function(error, hashedPassword) {
+            return callback(error, hashedPassword);
+        });
+    });
+};
+
+UserSchema.statics.passwordConfirm = function(password, confirmPassword) {
+    return password === confirmPassword;
+};
+
+/*
+ * Default authentication check. If no session exists, redirect to sign in page.
+ */
+UserSchema.statics.isAuthenticated = function(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/sign-in');
+};
+
+UserSchema.pre('save', function(next) {
+    var user = this;
+    UserSchema.statics.hashPassword(user.password, function(error, hashedPassword) {
+        if (error) {
+            next(error);
+        }
+        user.password = hashedPassword;
+        next();
+    });
+});
 
 var User = mongoose.model('User', UserSchema);
 module.exports = User;
