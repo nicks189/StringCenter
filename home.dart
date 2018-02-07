a2d9c86d2d2a64291bb7610bf9d05da192b0ce9c
Jacob Heiliger <heiliger@iastate.edu>
2018-02-07 17:41:15 -0600

    renamed home_screen.dart to home.dart

---
+++ home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  _HomeState createState() => new _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('HomeScreen'),
+      ),
+    );
+  }
+}
