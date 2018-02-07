e6db81eaf023aa0fe7533b069fc3eb7709dc9c0e
nicks189 <nsteger@iastate.edu>
2018-02-07 13:55:40 -0600

    Add route to return all users

--- StringCenterNode/routes/api/getUser.js
+++ StringCenterNode/routes/api/getUser.js
@@ -7,14 +7,28 @@ module.exports = function(passport) {
     /*
      * TODO: add authentication
      */
+    router.get('/', function(req, res, next) {
+        User.find({}, function(error, users) {
+            if (error) {
+                res.json({ error: 'Something went wrong' }).status(500);
+            } else if (users.length === 0) {
+                res.json({ error: 'No users found'}).status(400);
+            } else {
+                res.json({ users: users });
+            }
+        });
+    });
+
+    /*
+     * TODO: add authentication
+     */
     router.get('/:username', function(req, res, next) {
         User.findOne({ 'username': req.params.username}, function(error, user) {
             if (error) {
-                res.json({ error: 'Sign in failed' }).status(500);
+                res.json({ error: 'Something went wrong' }).status(500);
             } else if (!user) {
                 res.json({ error: 'Username not found'}).status(400);
             } else {
-                console.log(user);
                 res.json(user);
             }
         });
