a5600ba4aa50518901b4206d359e39e96f9995ed
Jacob Heiliger <heiliger@iastate.edu>
2018-02-07 17:27:19 -0600

    made HomeScreen the main route

--- main.dart
+++ main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'home_screen.dart';
+import 'tab.dart';
+
 void main() {
   runApp(new StringCenter());
 }
@@ -13,16 +16,11 @@ class _StringCenterState extends State<StringCenter> {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'StringCenter Home',
+      title: 'StringCenter',
       routes: <String, WidgetBuilder> {
-        //'home':(BuildContext context) => new Home(),
-
+        'home':(BuildContext context) => new HomeScreen(),
       },
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('StringCenter Home'),
-        ),
-      ),
+      home: new HomeScreen(),
     );
   }
 }
