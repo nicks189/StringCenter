6e161347b11bdca7ba69411ebad8df3b9ae93cee
Nick Steger <nsteger@iastate.edu>
2018-02-07 17:30:41 -0600

    Body parser is now using application/json

--- StringCenterNode/app.js
+++ StringCenterNode/app.js
@@ -43,7 +43,7 @@ app.set('view engine', 'pug');
 
 // body parser setup for parsing requests
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ type: 'application/json' }));
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
