9dffd651f283d7e876f95ef5075b9ec978ab1e00
mattb <mattbechtel123@gmail.com>
2018-02-06 21:23:53 -0600

    created a basic user model, going to try and link a foreign key to the tab table so that users are tied with their tabs

--- stringCenter/test/tabModel.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var TabSchema = new Schema({
-  tab: {type: Object, required: true},
-});
-
-module.exports = mongoose.model('Tab', TabSchema);
