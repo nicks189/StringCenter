c45e19c4e322876a8efac7161fb2ebf8c6ea91a7
mattb <mattbechtel123@gmail.com>
2018-02-10 18:26:04 -0600

    commit to be merged

--- stringCenter/routes/index.js
+++ stringCenter/routes/index.js
@@ -6,25 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/tabCreate', function(req, res, next){
-  //tabCreate(req);
-  console.log("request for tabCreate");
-});
-
-function tabCreate(tab){
-  //TabFromKeyboard.enterInformation();
-  console.log(tab);
-  var tabDetail = {tab: tab};
-  var tabModel = new Tab(tabDetail);
-  tabModel.save(function(err){
-    if(err){
-      console.log("ERROR" + err);
-      cb(err, null);
-    } else{
-      console.log(tab);
-      cb(null, tab);
-    }
-  })
-}
-
 module.exports = router;
