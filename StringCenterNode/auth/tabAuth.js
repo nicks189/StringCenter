d3cd4462a1484f3957b103ee3798b4cd5f3da274
mattb <mattbechtel123@gmail.com>
2018-02-12 22:17:15 -0600

    beginning planning on tab verification in order to make sure that malicious users cannot store tabs that are unrenderable on the client side

---
+++ StringCenterNode/auth/tabAuth.js
@@ -0,0 +1,14 @@
+var tabObj = {"info":"This is a new tab","measureCount":"1","measures":[{"info":"first measure","tuning":"EADG","stringCount":"4","strings":[{"notes":["-","-","-","-"],"tuning":"E"},{"notes":["-","-","-","-"],"tuning":"A"},{"notes":["-","-","-","-"],"tuning":"D"},{"notes":["-","-","-","-"],"tuning":"G"}]}]};
+
+
+function validateTab(tab){
+  //check info
+  //check tuning
+  //check actual measure count matches measureCount
+  //validate each measure
+    //check info
+    //check tuning
+    //validate that actual string count matches stringCount
+    //validate that string tuning matches tuning
+    //validate that noteCount matches for every string
+}
