81f98ee07d6c6977871e4766a66c5d45f610fb86
Jacob Heiliger <heiliger@iastate.edu>
2018-02-14 22:48:44 -0600

    changed the UI of create_measure to be a GridView

--- create_measure.dart
+++ create_measure.dart
@@ -3,21 +3,39 @@ import 'package:flutter/material.dart';
 import 'tab.dart';
 
 class CreateMeasure extends StatefulWidget {
-  Tabb _t;
-  CreateMeasure(Tabb t) {
-    _t = t;
+  String _title;
+  String _info;
+  String _tuning;
+
+  int i = 3;
+
+  CreateMeasure(String title, String info, String tuning) {
+    _title = title;
+    _info = info;
+    _tuning = tuning;
   }
-  _CreateMeasureState createState() => new _CreateMeasureState(_t);
+
+  _CreateMeasureState createState() => new _CreateMeasureState(_title, _info, _tuning);
 }
 
 class _CreateMeasureState extends State<CreateMeasure> {
   Tabb _t;
+  Measure _m;
   
-  _CreateMeasureState(Tabb t) {
-    _t = t;
+  _CreateMeasureState(String title, String info, String tuning) {
+    Tabb t = new Tabb(title, info, tuning);
   }
 
-  
+  void _pressed() {
+    setState((){
+
+    });
+  }
+
+  List<Widget> generateNotes() {
+    List<Widget> wlist = [];
+    return wlist;
+  }
   
   @override
   Widget build(BuildContext context) {
@@ -26,23 +44,16 @@ class _CreateMeasureState extends State<CreateMeasure> {
         title: new Text('Create Measure'),
       ),
       body: new Container(
-        child: new Center(
-          child: new Column(
-            children: <Widget>[
-              new Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  new ButtonBar(
-                    children: <Widget>[
+        alignment: Alignment.center,
+        child: new GridView.count(
+          primary: false,
+          scrollDirection: Axis.horizontal,
+          padding: const EdgeInsets.all(20.0),
+          mainAxisSpacing: 0.0,
+          crossAxisSpacing: 1.0,
+          crossAxisCount: 6,
 
-                    ],
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
+        )
       ),
     );
   }
