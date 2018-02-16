45a588bb2fa87f0a18991d2e1e2a89d8977f9a9f
Nick Steger <nsteger@iastate.edu>
2018-02-16 12:25:19 -0600

    Added sessions back for demo site

--- StringCenterNode/app.js
+++ StringCenterNode/app.js
@@ -29,12 +29,12 @@ app.use(expressSession({
         mongooseConnection: mongoose.connection
     })
 }));
-app.use(cookieParser('top-secret'));
+app.use(cookieParser(config.session.key));
 app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.session());
 
-// var userAuth = require('./auth/userAuth');
-// userAuth(passport);
+var userAuth = require('./auth/userAuth');
+userAuth(passport);
 
 var apiAuth = require('./auth/apiAuth');
 apiAuth(passport);
