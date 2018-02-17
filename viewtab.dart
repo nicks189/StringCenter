661e3675ff045a2a842b6d7e047c6e6bca0ea31a
Ben Ehlers <ehlersb@iastate.edu>
2018-02-17 14:18:15 -0600

    viewtab code

---
+++ viewtab.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import "dart:convert";
+import "dart:io";
+import 'tab.dart';
+
+class viewTab extends StatelessWidget {
+
+  _getTab() async {
+
+    try{
+
+    } catch(exception) {
+
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text('View'),),
+      body: new Container(
+        padding: new EdgeInsets.all(32.0),
+        child: new Center(
+          child: new Column(
+
+          ),
+        ),
+      ),
+    );
+  }
+
+}
