9caa194736fa739954ea08e95fac296bb61a03a2
Nick Steger <nsteger@iastate.edu>
2018-02-18 13:34:37 -0600

    Add error if username is already taken

--- StringCenterNode/routes/api/user/register.js
+++ StringCenterNode/routes/api/user/register.js
@@ -6,21 +6,28 @@ module.exports = function(passport) {
 
     router.post('/', function(req, res, next) {
         if (req.body.username && req.body.password) {
-            if (req.body.password !== req.body.confirmPassword) {
-                return res.json({ errors: [{ message: 'Passwords don\'t match' }] }).status(400);
-            }
-            var newUser = new User();
-            newUser.username = req.body.username;
-            newUser.password = req.body.password;
-            newUser.firstName = req.body.firstName;
-            newUser.lastName = req.body.lastName;
-            console.log(newUser);
-            newUser.validateAndSave(function (errors, user) {
-                if (errors) {
-                    console.log(errors);
-                    return res.json(errors).status(400);
+            User.findOne({ 'username': req.body.username }, function(error, user) {
+                if (error) {
+                    return res.json({ errors: [{ message: 'Something went wrong' }] }).status(500);
+                } else if (user) {
+                    return res.json({ errors: [{ message: 'Username is already taken' }] }).status(400);
                 }
-                res.json(user).status(201);
+                if (req.body.password !== req.body.confirmPassword) {
+                    return res.json({ errors: [{ message: 'Passwords don\'t match' }] }).status(400);
+                }
+                var newUser = new User();
+                newUser.username = req.body.username;
+                newUser.password = req.body.password;
+                newUser.firstName = req.body.firstName;
+                newUser.lastName = req.body.lastName;
+                console.log(newUser);
+                newUser.validateAndSave(function(errors, user) {
+                    if (errors) {
+                        console.log(errors);
+                        return res.json(errors).status(400);
+                    }
+                    res.json(user).status(201);
+                });
             });
         } else {
             res.json({ errors: [{ message: 'Invalid request' }] }).status(400);
