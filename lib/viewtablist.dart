ff5ec3e0fcdc86298daf4dfd71e228f7eff92042
Ben Ehlers <ehlersb@iastate.edu>
2018-02-17 19:20:35 -0600

    tab list view

--- lib/viewtablist.dart
+++ lib/viewtablist.dart
@@ -4,22 +4,16 @@ import "dart:io";
 import 'tab.dart';
 import 'viewtab.dart';
 
-class viewTabList extends StatelessWidget {
+class viewTabList extends StatefulWidget {
 
-  List<Tabb> _tabs = new List<Tabb>();
-  List<Widget> _wl = new List<Widget>();
+  _viewTabListState createState() => new _viewTabListState();
+}
 
-  /*viewTabList(List<Tabb> tabs, List<Widget> wl) {
-    _tabs = tabs;
-    _wl = wl;
-  }*/
-  _gotoTab(String title) {
+  class _viewTabListState extends State<viewTabList> {
 
-  }
- // _viewTabListState createState() => new _viewTabListState();
-
-
-  /*class _viewTabListState extends State<viewTabList>*/
+    bool _loaded = false;
+    List<Tabb> _tabs = new List<Tabb>();
+    List<Widget> _wl = new List<Widget>();
 
   _getTabList() async {//'http://proj-309-ss-5.cs.iastate.edu:3000/api/tab/findTabsByAuthorName/:author_username' //
     var url = 'http://proj-309-ss-5.cs.iastate.edu:3000/api/tab/findTabsByUser';
@@ -27,34 +21,57 @@ class viewTabList extends StatelessWidget {
     try {
       var request = await httpClient.getUrl(Uri.parse(url));
       request.headers.contentType = new ContentType("application", "json", charset: "utf-8");
+      request.headers.add("authorization", "bearer eyJhbGciOiJIUzI1NiJ9.NWE4ODlkYWUyZjkzOGMyZDAxZjVkMjdl.3s6PeRjyZUPSgHi3whRkS2-iGQJyvyZ-snauby7XvWE");
+
       var response = await request.close();
       var json = await response.transform(UTF8.decoder).join();
+      Map jsonD = JSON.decode(json);
       print(json);
-      //_tabs.add();
+      print(jsonD['tabs'].length);
+      for (int i = 0; i < jsonD['tabs'].length;i++) {
+        _tabs.add(new Tabb.fromJson(jsonD['tabs'][i]));
+      }
+      //_tabs.add(JSON.decode(json));
+      _generateWidgets();
 
 
     } catch(exception) {
       print('exception viewtablist');
     }
+
+
+
+  }
+
+  List<Widget> _generateWidgets() {
+
+    print(_tabs.length);
+    for (int i = 0; i < _tabs.length ;i ++){
+      //TODO put code for populating widget list
+      _wl.add(new MaterialButton(onPressed: () {
+        Navigator.push(context, new MaterialPageRoute(builder:(BuildContext context) => new viewTab(_tabs[i])));
+      },
+        child: new Text("Hello"),
+      ));
+
+    }
+  return _wl;
   }
 
   @override
   Widget build(BuildContext context) {
-    _getTabList();
-    for (int i = 0; i < _tabs.length ;i ++){
-    //TODO put code for populating widget list
-    _wl.add(new MaterialButton(onPressed: () {
-      Navigator.push(context, new MaterialPageRoute(builder:(BuildContext context) => new viewTab(_tabs[i])));
-    },));
-
+    if(_loaded == false) {
+      _loaded = true;
+      _getTabList();
     }
-
+    print("tabs: " + _tabs.toString());
+    print("widgets: " + _wl.toString());
     return new Scaffold(
       appBar: new AppBar(title: new Text('View Tab List'),),
       body: new Container(
         padding: new EdgeInsets.all(32.0),
         child: new Center(
-          child: new ListView(children: _wl),
+          child: new Column(children: _wl),
         ),
       ),
     );
