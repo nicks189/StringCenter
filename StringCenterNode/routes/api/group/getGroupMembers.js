0a9a2680c0ec5b27c2147115a607b75413577a75
mattb <mattbechtel123@gmail.com>
2018-02-24 15:39:59 -0600

    added getGroupMembers.js to query the database by groupName and return all the usernames in that group

--- StringCenterNode/routes/api/group/getGroupMembers.js
+++ StringCenterNode/routes/api/group/getGroupMembers.js
@@ -1,9 +1,23 @@
 var express = require('express');
+var UserGroup = require('../../../models/userGroup');
 
 
 module.exports = function(passport){
     var router = express.Router();
-    //routes
+
+    //queries by requested groupName and returns all usernames in that group
+    //authentication removed for testing
+    router.post('/', function(req, res, next){
+        if(req.body.groupName){
+            UserGroup.find({"groupName": req.body.groupName}, function(error, userGroup){
+                let usernames = [];
+                userGroup.forEach(function(e){
+                    usernames.push(e.username);
+                });
+                res.json({users : usernames});
+            });
+        }
+    })
 
     return router;
 }
