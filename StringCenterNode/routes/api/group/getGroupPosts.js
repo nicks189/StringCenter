3e9f427ff41710eb74fde8e2566a13f50c754ce7
mattb <mattbechtel123@gmail.com>
2018-02-24 16:29:25 -0600

    added getGroupPost queries in order to query posts based on groupName

--- StringCenterNode/routes/api/group/getGroupPosts.js
+++ StringCenterNode/routes/api/group/getGroupPosts.js
@@ -5,9 +5,15 @@ module.exports = function(passport){
     var router = express.Router();
     //routes
 
+    //authentication removed for testing
+    //queries posts by groupName
     router.post('/', function(req, res, next){
-        
-    })
+        if(req.body.groupName){
+            Post.find({"groupName" : req.body.groupName}).sort({"timestamp" : 'ascending'}).exec(function(error, post){
+                res.json(post);
+            });
+        }
+    });
 
 
     return router;
