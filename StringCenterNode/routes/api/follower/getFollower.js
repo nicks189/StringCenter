de0b3c2775fdc95c94175824ce11a71a70762e55
Nick Steger <nsteger@iastate.edu>
2018-02-25 10:59:39 -0600

    Can now get followers/following of other users

--- StringCenterNode/routes/api/follower/getFollower.js
+++ StringCenterNode/routes/api/follower/getFollower.js
@@ -5,7 +5,7 @@ module.exports = function(passport) {
     var router = express.Router();
 
     /*
-     * --- Get followers of the currently authenticated user ---
+     * --- Get all users the currently authenticated user follows ---
      */
     router.get('/following', passport.authenticate('jwt', { session: false }), function(req, res, next) {
         UserFollows.find({ 'username': req.user.username}, function(error, userFollows) {
@@ -23,10 +23,46 @@ module.exports = function(passport) {
     });
 
     /*
-     * --- Get all users the currently authenticated user follows ---
+     * --- Get all users that the user specified by username follows ---
+     */
+    router.get('/following/:username', passport.authenticate('jwt', { session: false }), function(req, res, next) {
+        UserFollows.find({ 'username': req.params.username}, function(error, userFollows) {
+            if (error) {
+                return res.json({ errors: [{ message: 'Something went wrong' }] }).status(500);
+            } else if (userFollows.length === 0) {
+                return res.json({ errors: [{ message: 'Not following anyone' }] }).status(400);
+            }
+            let following = [];
+            userFollows.forEach(function(e) {
+                 following.push(e.followsUsername);
+            });
+            res.json({ following: following }).status(200);
+        });
+    });
+
+    /*
+     * --- Get followers of the currently authenticated user ---
      */
     router.get('/followers', passport.authenticate('jwt', { session: false }), function(req, res, next) {
         UserFollows.find({ 'followsUsername': req.user.username}, function(error, userFollows) {
+            if (error) {
+                return res.json({ errors: [{ message: 'Something went wrong' }] }).status(500);
+            } else if (userFollows.length === 0) {
+                return res.json({ errors: [{ message: 'No followers' }] }).status(400);
+            }
+            let followers = [];
+            userFollows.forEach(function(e) {
+                 followers.push(e.username);
+            });
+            res.json({ followers: followers }).status(200);
+        });
+    });
+
+    /*
+     * --- Get followers of the user specified by username ---
+     */
+    router.get('/followers/:username', passport.authenticate('jwt', { session: false }), function(req, res, next) {
+        UserFollows.find({ 'followsUsername': req.params.username}, function(error, userFollows) {
             if (error) {
                 return res.json({ errors: [{ message: 'Something went wrong' }] }).status(500);
             } else if (userFollows.length === 0) {
