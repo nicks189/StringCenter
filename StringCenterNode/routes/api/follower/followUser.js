d296f275a330c35e04eb15e0d0333016d16eecf5
Nick Steger <nsteger@iastate.edu>
2018-02-25 12:04:32 -0600

    Users can no longer follow themselves

--- StringCenterNode/routes/api/follower/followUser.js
+++ StringCenterNode/routes/api/follower/followUser.js
@@ -5,28 +5,27 @@ var User = require('../../../models/user');
 module.exports = function(passport){
     var router = express.Router();
 
-    router.post('/', passport.authenticate('jwt', { session: false }), function(req, res, next){
-        if (req.body.followsUsername) {
-            UserFollows.findOne({ 'username': req.user.username,
-                'followsUsername': req.body.followsUsername }, function(error, userFollows) {
-                if (error) {
-                    return res.json({ errors: [{ message: 'Something went wrong' }] }).status(500);
-                } else if (userFollows) {
-                    return res.json({ errors: [{ message: 'Already following user' }] }).status(400);
-                }
-                let newUserFollows = new UserFollows();
-                newUserFollows.username = req.user.username;
-                newUserFollows.followsUsername = req.body.followsUsername;
-                newUserFollows.validateAndSave(function(err, saved) {
-                    if (err) {
-                        return res.json({ errors: [{ message: 'Something went wrong' }] }).status(500);
-                    }
-                    res.json(saved).status(201);
-                });
-            });
-        } else {
-            return res.json({ errors: [{ message: 'Invalid request' }] }).status(400);
+    router.post('/:username', passport.authenticate('jwt', { session: false }), function(req, res, next){
+        if (req.user.username === req.params.username) {
+            return res.json({ errors: [{ message: 'Cannot follow yourself' }] }).status(400);
         }
+        UserFollows.findOne({ 'username': req.user.username,
+            'followsUsername': req.params.username }, function(error, userFollows) {
+            if (error) {
+                return res.json({ errors: [{ message: 'Something went wrong' }] }).status(500);
+            } else if (userFollows) {
+                return res.json({ errors: [{ message: 'Already following user' }] }).status(400);
+            }
+            let newUserFollows = new UserFollows();
+            newUserFollows.username = req.user.username;
+            newUserFollows.followsUsername = req.params.username;
+            newUserFollows.validateAndSave(function(err, saved) {
+                if (err) {
+                    return res.json({ errors: [{ message: 'Something went wrong' }] }).status(500);
+                }
+                res.json(saved).status(201);
+            });
+        });
     });
     return router;
 };
