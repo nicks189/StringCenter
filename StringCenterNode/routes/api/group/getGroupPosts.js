296a97646e22974c828a57edfb96c29dd95af30a
mattb <mattbechtel123@gmail.com>
2018-03-02 14:36:57 -0600

    added sorting to getGroupPosts so that the posts return newest to oldest, in regard to timestamp

--- StringCenterNode/routes/api/group/getGroupPosts.js
+++ StringCenterNode/routes/api/group/getGroupPosts.js
@@ -6,11 +6,15 @@ module.exports = function(passport){
     //routes
 
     //authentication removed for testing
-    //queries posts by groupName
+    //queries posts by groupName and sorts them by timestamp
     router.post('/', function(req, res, next){
         if(req.body.groupName){
-            Post.find({"groupName" : req.body.groupName}).sort({"timestamp" : 'ascending'}).exec(function(error, post){
-                res.json(post);
+            Post.find({"groupName" : req.body.groupName}, function(error, posts){
+                posts.sort(function(a, b){
+                    return new Date(b.timestamp) - new Date(a.timestamp);
+                });
+
+                res.json(posts);
             });
         }
     });
