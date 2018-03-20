510c9da7f05c5791d85b866a507db6af8219ff77
Nick Steger <nsteger@iastate.edu>
2018-03-20 14:00:35 -0500

    Now validating that the user exists

--- StringCenterNode/routes/api/follower/followUser.js
+++ StringCenterNode/routes/api/follower/followUser.js
@@ -9,21 +9,28 @@ module.exports = function(passport){
         if (req.user.username === req.params.username) {
             return res.json({ errors: [{ message: 'Cannot follow yourself' }] }).status(400);
         }
-        UserFollows.findOne({ username: req.user.username,
-            followsUsername: req.params.username }, function(error, userFollows) {
+        User.findOne({ username: req.params.username }, function(error, user) {
             if (error) {
                 return res.json({ errors: [{ message: 'Something went wrong' }] }).status(500);
-            } else if (userFollows) {
-                return res.json({ errors: [{ message: 'Already following user' }] }).status(400);
+            } else if (!user) {
+                return res.json({ errors: [{ message: 'User doesn\'t exist' }] }).status(400);
             }
-            let newUserFollows = new UserFollows();
-            newUserFollows.username = req.user.username;
-            newUserFollows.followsUsername = req.params.username;
-            newUserFollows.validateAndSave(function(err, saved) {
-                if (err) {
+            UserFollows.findOne({ username: req.user.username,
+                followsUsername: req.params.username }, function(error, userFollows) {
+                if (error) {
                     return res.json({ errors: [{ message: 'Something went wrong' }] }).status(500);
+                } else if (userFollows) {
+                    return res.json({ errors: [{ message: 'Already following user' }] }).status(400);
                 }
-                res.json(saved).status(201);
+                let newUserFollows = new UserFollows();
+                newUserFollows.username = req.user.username;
+                newUserFollows.followsUsername = req.params.username;
+                newUserFollows.validateAndSave(function(err, saved) {
+                    if (err) {
+                        return res.json({ errors: [{ message: 'Something went wrong' }] }).status(500);
+                    }
+                    res.json(saved).status(201);
+                });
             });
         });
     });
