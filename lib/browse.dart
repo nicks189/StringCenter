0a22e3a9b2c170dcda89fbfd582a2e3a18b077aa
Ben Ehlers <ehlersb@iastate.edu>
2018-03-22 12:58:05 -0500

    browse is nearly finished, needs to be tested. Might get the backend to make a query to help get posts of a particular user. made viewUser for viewing the page of a particular user

--- lib/browse.dart
+++ lib/browse.dart
@@ -6,6 +6,9 @@ import 'dart:convert';
 import 'tab.dart';
 import 'group_object.dart';
 import 'user.dart';
+import 'viewtab.dart';
+import 'group_page.dart';
+import 'viewUser.dart';
 
 class Browse extends StatefulWidget {
 
@@ -21,7 +24,7 @@ class _BrowseState extends State<Browse> {
   List<Tabb> _tabList = new List<Tabb>();
   List<User> _userList = new List<User>();
   List<Group> _groupList = new List<Group>();
-  
+
   _getTabList() async {
     var url = "http://proj-309-ss-5.cs.iastate.edu:3000/api/tab/";
     try {
@@ -69,61 +72,45 @@ class _BrowseState extends State<Browse> {
     }
   }
 
-  List<Widget> _genTabWidgets() {
-
-  }
-  List<Widget> _genGroupWidgets() {
-
-  }
-  List<Widget> _genUserWidgets() {
-
-  }
-  List<Widget> _generateWidgets() { //TODO need 3 genwidgets
-
-    List<Widget> widgetList = new List<Widget>();
-    widgetList.add( new Container(
-        margin: new EdgeInsets.all(24.0),
-        padding: new EdgeInsets.all(12.0),
-        decoration: new BoxDecoration(
-          border: new Border.all(color: Colors.blue, width: 2.0),
-          borderRadius: new BorderRadius.all(new Radius.circular(6.0)),
-
-        ),
-        child: new Text("new group description test test description new new test "
-            "new description new new test description new test new test")
-    ),
-    );
-
-    widgetList.add(new Container(
-      child: new MaterialButton(
-          minWidth: 128.0,
-          height: 32.0,
-          child: new Text("Create Post", style: new TextStyle(color: Colors.white, fontSize: 16.0),),
-          onPressed: () {
-            Navigator.push(context, new MaterialPageRoute(builder:(BuildContext context) => new Scaffold(
-              body: new Text("Post Creation Screen"),
-            )));
-          }
-      ),
-      margin: new EdgeInsets.all(24.0),
-      decoration: new BoxDecoration(
-        border: new Border.all(color: Colors.red),
-        borderRadius: new BorderRadius.all(new Radius.circular(6.0)),
-        color: Colors.red,
-      ),
-    ));
-    for (int i = 0; i < _postList.length; i++) {
+  _genTabWidgets() {
+  List<Widget> widgetList = new List<Widget>();
+    for(int i = 0; i < _tabList.length; i++) {
       widgetList.add(new MaterialButton(onPressed: () {
-        Navigator.push(context, new MaterialPageRoute(builder:(BuildContext context) => new Scaffold(
-          body: new Text(_postList[i].content),
-        )));
+        Navigator.push(context, new MaterialPageRoute(builder:(BuildContext context) => new ViewTab(_tabList[i])));
       },
-        child: new Text(_postList[i].title),
-      )
-      );
+      child: new Text(_tabList[i].title),
+      ));
+   }
+    setState(() {
+     _widgetList = widgetList;
+    });
+  }
+   _genGroupWidgets() {
+    List<Widget> widgetList = new List<Widget>();
+    for(int i = 0; i < _groupList.length; i++) {
+      widgetList.add(new MaterialButton(onPressed: () {
+        Navigator.push(context, new MaterialPageRoute(builder:(BuildContext context) => new GroupPage(_groupList[i].groupName)));
+      },
+        child: new Text(_groupList[i].groupName),
+      ));
     }
-    return widgetList;
-  } //_generateWidgets
+    setState(() {
+      _widgetList = widgetList;
+    });
+  }
+  _genUserWidgets() { //TODO make viewUser and edit accordingly
+    List<Widget> widgetList = new List<Widget>();
+    for(int i = 0; i < _groupList.length; i++) {
+      widgetList.add(new MaterialButton(onPressed: () {
+        Navigator.push(context, new MaterialPageRoute(builder:(BuildContext context) => new GroupPage(_groupList[i].groupName)));
+      },
+        child: new Text(_groupList[i].groupName),
+      ));
+    }
+    setState(() {
+      _widgetList = widgetList;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -141,18 +128,25 @@ class _BrowseState extends State<Browse> {
                   new RaisedButton(
                     child: new Text("Tabs"),
                     onPressed: () {
+                      _getTabList().then(() {
+                        _genTabWidgets();
+                      });
                     },
                   ),
                   new RaisedButton(
                     child: new Text("Groups"),
                     onPressed: () {
-
+                      _getGroupList().then(() {
+                        _genTabWidgets();
+                      });
                     },
                   ),
                   new RaisedButton(
                     child: new Text("Users"),
                     onPressed: () {
-
+                      _getUserList().then(() {
+                        _genTabWidgets();
+                      });
                     },
                   ),
                 ],
---
+++ lib/viewUser.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/services.dart';
+import 'dart:convert';
+import 'servercommunication.dart';
+import 'post.dart';
+
+
+import 'globals.dart' as globals;
+
+class ViewUser extends StatefulWidget {
+  String _givenUserName;
+  ViewUser(String givenUserName){
+    _givenUserName = givenUserName;
+  }
+  @override
+  _ViewUserState createState() =>
+      new _ViewUserState(_givenUserName);
+}
+
+class _ViewUserState extends State<ViewUser> {
+  String _userName;
+  Image i = new Image.asset('einstein.jpg');
+  List<Widget> _widgetList = new List<Widget>();
+  List<Post> _postList = new List<Post>();
+  _ViewUserState(String givenUserName) {
+    _userName = givenUserName;
+  }
+
+  _getPostList() async {
+    var url ="http://proj-309-ss-5.cs.iastate.edu:3000/api/get-post/by-user"; //TODO i was working on this
+    try {
+      String responseBody = await getRequestTokenAuthorization(url, globals.token);
+      Map posts = json.decode(responseBody);
+
+      print("posts.length: "+ posts['posts'].length.toString());
+      for(int i = 0; i < posts['posts'].length; i++) {
+        _postList.add(new Post(posts['posts'][i]["title"], posts['posts'][i]["content"]));
+      }
+    } catch(exception) {
+      print("profilepage exception: " + exception.toString());
+    }
+  }
+
+  @override
+  void initState() {
+    _getPostList().then((widgetList) {
+      widgetList = _generateWidgets();
+      setState(() {
+        _widgetList = widgetList;
+      });
+    });
+  }
+
+  List<Widget> _generateWidgets() {
+    List<Widget> widgetList = new List<Widget>();
+    widgetList.add(new Text('About Me:', textAlign: TextAlign.left, style: new TextStyle(fontSize: 20.0),));
+    widgetList.add( new Container(
+        margin: new EdgeInsets.all(12.0),
+        padding: new EdgeInsets.all(12.0),
+        decoration: new BoxDecoration(
+          border: new Border.all(color: Colors.blue, width: 2.0),
+          borderRadius: new BorderRadius.all(new Radius.circular(6.0)),
+
+        ),
+        child: new Text(globals.user.username)
+    ),
+    );
+    widgetList.add(new Container(
+      child: new MaterialButton(
+          minWidth: 128.0,
+          height: 32.0,
+          child: new Text("Create Post", style: new TextStyle(color: Colors.white, fontSize: 16.0),),
+          onPressed: () {
+            Navigator.push(context, new MaterialPageRoute(builder:(BuildContext context) => new Scaffold(
+              body: new Text("Post Creation Screen"),
+            )));
+          }
+      ),
+      margin: new EdgeInsets.all(24.0),
+      decoration: new BoxDecoration(
+        border: new Border.all(color: Colors.red),
+        borderRadius: new BorderRadius.all(new Radius.circular(6.0)),
+        color: Colors.red,
+      ),
+    ));
+    for (int i = 0; i < _postList.length; i++) {
+      widgetList.add(new MaterialButton(onPressed: () {
+        Navigator.push(context, new MaterialPageRoute(builder:(BuildContext context) => new Scaffold(
+          body: new Text(_postList[i].content),
+        )));
+      },
+        child: new Text(_postList[i].title),
+      )
+      );
+    }
+    return widgetList;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    return new Scaffold(
+      appBar: new AppBar(
+        leading: new Image.asset('images/einstein.jpg', fit: BoxFit.scaleDown,),
+        title: new Text(globals.user.username),
+      ),
+      body: new Container(
+        alignment: Alignment.center,
+        padding: new EdgeInsets.all(20.0),
+        child: new ListView(
+          children: _widgetList,
+        ),
+      ),
+    );
+  }
+}
