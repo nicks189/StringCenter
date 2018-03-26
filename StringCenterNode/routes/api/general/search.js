58abc1cf0f0cbfe9c059296b3bbff074290bb828
Nick Steger <nsteger@iastate.edu>
2018-03-25 22:35:32 -0500

    Add api/search

--- StringCenterNode/routes/api/general/search.js
+++ StringCenterNode/routes/api/general/search.js
@@ -2,17 +2,36 @@ const express = require('express');
 const Post = require('../../../models/post');
 const User = require('../../../models/user');
 const Tab = require('../../../models/tabModel');
+const Group = require('../../../models/group');
 
 module.exports = function(passport) {
     let router = express.Router();
 
-    router.post('/', passport.authenticate('jwt', { session: false }), function(req, res, next) {
-        if (req.body.query) {
-            // TODO
-            return res.json({ message: 'Todo' }).status(200);
-        } else {
-            return res.json({ errors: [{ message: 'Invalid request' }] }).status(400);
-        }
+    // TODO: order by relevancy (in models)
+    router.get('/:query', passport.authenticate('jwt', { session: false }), function(req, res, next) {
+        // search by regular expression of 'query', 'i' flag ignores case
+        let regex = new RegExp(req.params.query, 'i');
+        User.search(regex, function(error, users) {
+            if (error) {
+                return res.json({ errors: [{ message: 'Something went wrong' }] }).status(500);
+            }
+            Group.search(regex, function(error, groups) {
+                if (error) {
+                    return res.json({ errors: [{ message: 'Something went wrong' }] }).status(500);
+                }
+                Post.search(regex, function(error, posts) {
+                    if (error) {
+                        return res.json({ errors: [{ message: 'Something went wrong' }] }).status(500);
+                    }
+                    Tab.search(regex, function(error, tabs) {
+                        if (error) {
+                            return res.json({ errors: [{ message: 'Something went wrong' }] }).status(500);
+                        }
+                        res.json({ users: users, groups: groups, posts: posts, tabs: tabs }).status(200);
+                    });
+                });
+            });
+        });
     });
 
     return router;
