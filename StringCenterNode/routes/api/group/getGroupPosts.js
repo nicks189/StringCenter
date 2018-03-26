4539c166d0f50923c7f7d7849672b2d5878c2cf2
mattb <mattbechtel123@gmail.com>
2018-03-26 15:50:08 -0500

    updated getGroupPosts to be a post request using groupName as the url parameter

--- StringCenterNode/routes/api/group/getGroupPosts.js
+++ StringCenterNode/routes/api/group/getGroupPosts.js
@@ -6,17 +6,17 @@ module.exports = function(passport){
     //routes
 
     //change to make the same as get user posts
-    
+
     //authentication removed for testing
     //queries posts by groupName and sorts them by timestamp
-    router.post('/', function(req, res, next){
-        if(req.body.groupName){
-            Post.find({"groupName" : req.body.groupName}, function(error, posts){
+    router.get('/:groupName', function(req, res, next){
+        if(req.params.groupName){
+            Post.find({"groupName" : req.params.groupName}, function(error, posts){
                 posts.sort(function(a, b){
                     return new Date(b.timestamp) - new Date(a.timestamp);
                 });
 
-                res.json(posts);
+                res.json({posts: posts}).status(200);
             });
         }
     });
