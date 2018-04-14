a2670b9be8c623f37919df017709bdf03514312b
Ben Ehlers <ehlersb@iastate.edu>
2018-04-14 14:31:19 -0500

    refactored home. fixed delete account

--- lib/home.dart
+++ lib/home.dart
@@ -23,6 +23,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   String _message;
+  bool _loaded = false;
   TextStyle buttonStyle = new TextStyle(color: Colors.white, fontFamily: 'times');
   Widget _page = new Container();
 
@@ -36,12 +37,12 @@ class _HomeState extends State<Home> {
 
   @override
   void initState() {
-    _requestUser().then((page) {
-      page = _generateWidgets();
-      setState(() {
-        _page = page;
-      });
-    });
+     _requestUser().then((_loaded){
+       setState(() {
+         _loaded = true;
+       });
+     });
+
   }
 
   _requestUser() async {
@@ -56,8 +57,8 @@ class _HomeState extends State<Home> {
       print("home" + responseBody);
       if (responseBody == "Unauthorized") {
         resetAuth();
-        _page = new Login();
         setState(() {
+          _loaded = true;
         });
       } else {
         globals.isLoggedIn = true;
@@ -65,162 +66,143 @@ class _HomeState extends State<Home> {
         User u = new User(m['username'], m['_id'], m['description'], m['adminStatus']);
         globals.user = u;
         globals.token = token;
+        setState((){
+          _loaded = true;
+        });
+
       }
       print("globals.isLoggedin (Home): " + globals.isLoggedIn.toString());
     } catch (exception) {
       resetAuth();
       print("requestuser exception: " + exception.toString());
-      _page = new Login();
       setState(() {
+        _loaded = true;
       });
     }
   } // end requestUser()
 
-  Widget _generateWidgets() {
-    if (!globals.isLoggedIn)
-      return new Login();
-    else
-      _page = new Scaffold(
-        appBar: new AppBar(
-          backgroundColor: globals.themeColor,
-          title: new Text('Home'),
-          actions: <Widget>[
-            new IconButton(
-                icon: new Icon(Icons.exit_to_app),
-                onPressed: () async {
-                  String dir = (await getApplicationDocumentsDirectory()).path;
-                  deleteFile("token.txt", dir);
-                  globals.isLoggedIn = false;
-                  globals.user = null;
-                  Navigator.of(context).pushNamedAndRemoveUntil(
-                      'Home', (Route<dynamic> route) => false);
-                })
-          ],
-        ),
-        body: new Container(
-          decoration: new BoxDecoration(
-            image: new DecorationImage(
-              image: new AssetImage("images/guitar.jpg"),
-              fit: BoxFit.cover,
-            ),
-
+  @override
+  Widget build(BuildContext context) {
+    if (!_loaded) {
+      return new Container();
+    }
+    else {
+      print("globals.isLoggedin: " + globals.isLoggedIn.toString());
+      if (!globals.isLoggedIn)
+        return new Login();
+      else
+        return new Scaffold(
+          appBar: new AppBar(
+            backgroundColor: globals.themeColor,
+            title: new Text('Home'),
+            actions: <Widget>[
+              new IconButton(
+                  icon: new Icon(Icons.exit_to_app),
+                  onPressed: () async {
+                    String dir = (await getApplicationDocumentsDirectory())
+                        .path;
+                    deleteFile("token.txt", dir);
+                    globals.isLoggedIn = false;
+                    globals.user = null;
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                        'Home', (Route<dynamic> route) => false);
+                  })
+            ],
           ),
-          padding: new EdgeInsets.all(32.0),
-          child: new Center(
-            child: new ListView(
-              itemExtent: 30.0,
-              children: <Widget>[
+          body: new Container(
+            decoration: new BoxDecoration(
+              image: new DecorationImage(
+                image: new AssetImage("images/guitar.jpg"),
+                fit: BoxFit.cover,
+              ),
+
+            ),
+            padding: new EdgeInsets.all(32.0),
+            child: new Center(
+              child: new ListView(
+                itemExtent: 30.0,
+                children: <Widget>[
 //              new RaisedButton(
 //                child: new Text("Login"),
 //                onPressed: () {
 //                  Navigator.of(context).pushNamed('Login');
 //                },
 //             ),
-                new Padding(padding: new EdgeInsets.all(16.0)),
-                new RaisedButton(
-                  color: globals.themeColor,
-                  child: new Text(
-                    "View Tabs",
-                    style: buttonStyle,
+                  new Padding(padding: new EdgeInsets.all(16.0)),
+                  new RaisedButton(
+                    color: globals.themeColor,
+                    child: new Text(
+                      "View Tabs",
+                      style: buttonStyle,
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          new MaterialPageRoute(
+                            builder: (
+                                BuildContext context) => new ViewTabList(),
+                          ));
+                    },
                   ),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        new MaterialPageRoute(
-                          builder: (BuildContext context) => new ViewTabList(),
-                        ));
-                  },
-                ),
-                new Padding(padding: new EdgeInsets.all(16.0)),
-                new RaisedButton(
-                  color: globals.themeColor,
-                  child: new Text("Create Tab", style: buttonStyle,),
-                  onPressed: () {
-                    Navigator.of(context).pushNamed('TabOptions');
-                  },
-                ),
-                new Padding(padding: new EdgeInsets.all(16.0)),
-                new RaisedButton(
-                  color: globals.themeColor,
-                  child: new Text("Followers", style: buttonStyle,),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        new MaterialPageRoute(
-                            builder: (BuildContext context) =>
-                                new Followers(globals.user.username)));
-                  },
-                ),
-                new Padding(padding: new EdgeInsets.all(16.0)),
-                new RaisedButton(
-                  color: globals.themeColor,
-                  child: new Text("Following", style: buttonStyle,),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        new MaterialPageRoute(
-                            builder: (BuildContext context) =>
-                                new Following(globals.user.username)));
-                  },
-                ),
-                new Padding(padding: new EdgeInsets.all(16.0)),
-                new RaisedButton(
-                  color: globals.themeColor,
-                  child: new Text("Profile", style: buttonStyle,),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        new MaterialPageRoute(
-                            builder: (BuildContext context) => new Profile()));
-                  },
-                ),
-                new Padding(padding: new EdgeInsets.all(16.0)),
-                new RaisedButton(
-                  color: globals.themeColor,
-                  child: new Text("Browse", style: buttonStyle,),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        new MaterialPageRoute(
-                            builder: (BuildContext context) => new Browse()));
-                  },
-                ),
-                new Padding(padding: new EdgeInsets.all(16.0)),
-                new RaisedButton(
-                  color: globals.themeColor,
-                  child: new Text("Search", style: buttonStyle,),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        new MaterialPageRoute(
-                            builder: (BuildContext context) => new Search()));
-                  },
-                ),
-                new Padding(padding: new EdgeInsets.all(16.0)),
-                new RaisedButton(
-                  color: globals.themeColor,
-                  child: new Text("CreateGroup", style: buttonStyle,),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        new MaterialPageRoute(
-                            builder: (BuildContext context) => new CreateGroup()));
-                  },
-                ),
-              ],
+                  new Padding(padding: new EdgeInsets.all(16.0)),
+                  new RaisedButton(
+                    color: globals.themeColor,
+                    child: new Text("Create Tab", style: buttonStyle,),
+                    onPressed: () {
+                      Navigator.of(context).pushNamed('TabOptions');
+                    },
+                  ),
+                  new Padding(padding: new EdgeInsets.all(16.0)),
+                  new RaisedButton(
+                    color: globals.themeColor,
+                    child: new Text("Profile", style: buttonStyle,),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          new MaterialPageRoute(
+                              builder: (
+                                  BuildContext context) => new Profile()));
+                    },
+                  ),
+                  new Padding(padding: new EdgeInsets.all(16.0)),
+                  new RaisedButton(
+                    color: globals.themeColor,
+                    child: new Text("Browse", style: buttonStyle,),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          new MaterialPageRoute(
+                              builder: (BuildContext context) => new Browse()));
+                    },
+                  ),
+                  new Padding(padding: new EdgeInsets.all(16.0)),
+                  new RaisedButton(
+                    color: globals.themeColor,
+                    child: new Text("Search", style: buttonStyle,),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          new MaterialPageRoute(
+                              builder: (BuildContext context) => new Search()));
+                    },
+                  ),
+                  new Padding(padding: new EdgeInsets.all(16.0)),
+                  new RaisedButton(
+                    color: globals.themeColor,
+                    child: new Text("CreateGroup", style: buttonStyle,),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          new MaterialPageRoute(
+                              builder: (
+                                  BuildContext context) => new CreateGroup()));
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-      );
-    return _page;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (_page.runtimeType == new Container().runtimeType) {
-      return new Container();
+        );
     }
-    print("globals.isLoggedin: " + globals.isLoggedIn.toString());
-    return _page;
-  } // end build
+  }// end build
 } // end class _HomeState
