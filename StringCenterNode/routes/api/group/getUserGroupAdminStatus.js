d08534efd358cee9e71a05e95088001937bf01d6
mattb <mattbechtel123@gmail.com>
2018-04-21 13:18:57 -0500

    Did a little refactoring of getUserGroupAdminStatus.js and put the authentication back for it

--- StringCenterNode/routes/api/group/getUserGroupAdminStatus.js
+++ StringCenterNode/routes/api/group/getUserGroupAdminStatus.js
@@ -13,24 +13,18 @@ var UserGroup = require('../../../models/userGroup');
  */
 function getUserGroupAdminStatus(passport){
     var router = express.Router();
-
-    router.get('/:username/:groupName', function(req, res, next){
+    router.get('/:username/:groupName', passport.authenticate('jwt', { session: false }), function(req, res, next){
         if(req.params.groupName && req.params.username){
             UserGroup.findOne({'groupName' : req.params.groupName, 'username' : req.params.username}, function(err, userGroup){
                 if(err){
                     return res.json({errors: [{message: 'Something went wrong in finding'}]}).status(500);
-                } else if(!userGroup){
-                    return res.json({errors: [{message : 'User is not a member of this group or the group doesn\'t exist'}]}).status(500);
-                } else if(userGroup){
-                    return res.json({adminStatus : userGroup.admin});
-                } else{
-                    return res.json({errors: [{message: 'Something went wrong'}]}).status(500);
                 }
+                return (userGroup ? res.json({adminStatus : userGroup.admin}) : res.json({errors: [{message : 'User is not a member of this group or the group doesn\'t exist'}]}).status(500));
             });
         }
     });
-
     return router;
 }
+
 
 module.exports = getUserGroupAdminStatus;
