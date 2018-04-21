38b49c06b139ceb976197efc72e5cb1f2f8901a7
mattb <mattbechtel123@gmail.com>
2018-04-21 13:09:24 -0500

    put authentication back for getGroupMemebers.js

--- StringCenterNode/routes/api/group/getGroupMembers.js
+++ StringCenterNode/routes/api/group/getGroupMembers.js
@@ -2,7 +2,6 @@ var express = require('express');
 var UserGroup = require('../../../models/userGroup');
 
 
-//authentication removed for testing
 /**
  * Returns usernames in alphabetical order for members in a Group with the matching groupName.
  * @param  {passport}       passport  used for authentication
@@ -13,25 +12,16 @@ var UserGroup = require('../../../models/userGroup');
  */
  function getGroupMembers(passport){
     var router = express.Router();
-
-    router.get('/:groupName', function(req, res, next){
+    router.get('/:groupName', passport.authenticate('jwt', { session: false }), function(req, res, next){
         if(req.params.groupName){
             UserGroup.find({"groupName": req.params.groupName}, function(error, userGroup){
                 let usernames = [];
-                userGroup.forEach(function(e){
-                    usernames.push(e.username);
-                });
-
-                //sort usernames alphabeticallybingbangboom
-                usernames = usernames.sort(function(a, b){
-                    return a.toLowerCase().localeCompare(b.toLowerCase());
-                });
-
+                userGroup.forEach(function(e){usernames.push(e.username);});
+                usernames = usernames.sort(function(a, b){return a.toLowerCase().localeCompare(b.toLowerCase());});
                 return (usernames.length > 0 ? res.json({ usernames : usernames }).status(200) : res.json({errors: [{message : 'no one in Group'}]}));
             });
         }
     });
-
     return router;
 }
 
