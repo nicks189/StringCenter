90b4af3080c5bec33301f7b387923d98b3b74858
mattb <mattbechtel123@gmail.com>
2018-04-22 16:17:55 -0500

    newsfeed now works, removed duplicate posts upon return

--- StringCenterNode/routes/api/general/newsfeed.js
+++ StringCenterNode/routes/api/general/newsfeed.js
@@ -26,17 +26,20 @@ function newsfeed(passport){
                         UserGroup.find({"username" : req.user.username}, function(ugerr, ug){
                             if (ugerr) return res.json({errors: [{message: 'Something went wrong in finding'}]}).status(500);
                             ug.forEach(function(ugs, j){
-
                                 Post.find({"groupName" : ugs.groupName}, function(pugerr, gPosts){
                                     if (pugerr) return res.json({errors: [{message: 'Something went wrong in finding'}]}).status(500);
                                     gPosts.forEach(function(gp){followsPosts.push(gp);});
                                     if(j == ug.length - 1){
-                                        let unique = [];
+                                        let uniqueIDs = [];
+                                        let uniquePosts = [];
                                         followsPosts.forEach(function(fp){
-                                            if(unique.indexOf(fp) == -1) unique.push(fp);
+                                            if(uniqueIDs.indexOf(String(fp._id)) == -1){
+                                                uniqueIDs.push(String(fp._id));
+                                                uniquePosts.push(fp);
+                                            }
                                         });
 
-                                        Post.buildPostList(followsPosts, function (error, ret) {
+                                        Post.buildPostList(uniquePosts, function (error, ret) {
                                             if (error) return res.json({errors: [{message: 'Something went wrong building the post list'}]}).status(500);
                                             return res.json({posts : ret}).status(200);
                                         });
