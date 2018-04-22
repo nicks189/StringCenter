c6240d9121c7d9a84fe1d541fb81a59ae64bdfdd
Ben Ehlers <ehlersb@iastate.edu>
2018-04-22 17:34:10 -0500

    changed order of post text and tab

--- lib/views/group_page.dart
+++ lib/views/group_page.dart
@@ -96,13 +96,13 @@ class _GroupPageState extends State<GroupPage> {
               },
               child: new Row(
                 children: <Widget>[
-                  new Container(
-                   constraints: new BoxConstraints(),
-                    padding: new EdgeInsets.fromLTRB(0.0,0.0,12.0,0.0),
-                    child: new Text(_postList[i].content,
-                        style: new TextStyle(fontSize: 12.0),
-                    softWrap: true,)),
                   new Text(_postList[i].tabRender()),
+                  new Container(
+                      constraints: new BoxConstraints(),
+                      padding: new EdgeInsets.fromLTRB(0.0,0.0,12.0,0.0),
+                      child: new Text(_postList[i].content,
+                        style: new TextStyle(fontSize: 12.0),
+                        softWrap: true,)),
                   /*new IconButton(icon: new Icon(Icons.zoom_in), onPressed: null)*/
                 ],
               )
