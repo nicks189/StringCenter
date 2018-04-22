0a9eae98f9d4284b341f4c48587d1a519d804bcc
Jacob Heiliger <heiliger@iastate.edu>
2018-04-22 17:16:59 -0500

    instantly updates new profile pic

--- lib/views/imagepick.dart
+++ lib/views/imagepick.dart
@@ -1,6 +1,9 @@
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
+import 'package:ss_5/data/user.dart';
+import 'dart:convert';
+import 'package:ss_5/util/globals.dart' as globals;
 import 'dart:async';
 import 'package:ss_5/communications/servercommunication.dart';
 
@@ -13,12 +16,22 @@ class _MyHomePageState extends State<MyHomePage> {
   File _image;
 
   Future getImage() async {
-    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
 
     setState(() {
       _image = image;
       Upload(_image);
     });
+    try {
+      var url =
+          "http://proj-309-ss-5.cs.iastate.edu:3000/api/get-user/info";
+      String responseBody = await getRequestAuthorization(url);
+      Map users = json.decode(responseBody);
+      globals.user.profilePic(users['profilePic']);
+    } catch (exception) {
+      print("viewUserList exception: " + exception.toString());
+    }
+
   }
 
   @override
