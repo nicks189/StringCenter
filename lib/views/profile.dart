ac5c5c34b9c2a8b537b1165f64c678b9f4401dfe
Ben Ehlers <ehlersb@iastate.edu>
2018-04-23 13:30:07 -0500

    reformatted code in viewtablist. Viewuser and profile post previews are complete

--- lib/views/profile.dart
+++ lib/views/profile.dart
@@ -12,7 +12,7 @@ import 'package:ss_5/views/create_post.dart';
 import 'package:ss_5/views/edit_profile.dart';
 import 'package:ss_5/views/view_post.dart';
 import 'package:ss_5/util/globals.dart' as globals;
-
+import 'package:ss_5/data/tab.dart';
 ///Profile is a StatefulWidget that is the profile page for the user
 ///that is currently logged in.
 class Profile extends StatefulWidget {
@@ -23,6 +23,12 @@ class _ProfileState extends State<Profile> {
   Image i = new Image.asset('einstein.jpg');
   List<Widget> _widgetList = new List<Widget>();
   List<Post> _postList = new List<Post>();
+
+  double _textBoxWidth(bool hasTab) {
+    if (hasTab) return 128.0;
+    else return 312.0;
+  }
+
 
   _getPostList() async {
     var url = "http://proj-309-ss-5.cs.iastate.edu:3000/api/get-post/by-user/current";
@@ -37,9 +43,13 @@ class _ProfileState extends State<Profile> {
         for (int i = 0; i < posts['posts'].length; i++) {
           if (posts['posts'][i]['tabId'] != null) {
             _postList.add(new Post(posts['posts'][i]['authorUsername'],
-                posts['posts'][i]["content"], posts['posts'][i]['tabId']));
+                posts['posts'][i]["content"],
+                posts['posts'][i]['tabId'],
+                posts['posts'][i]['groupName'],
+                Tabb.fromJson(posts['posts'][i]['tab'])));
           } else {
-            _postList.add(new Post(posts['posts'][i]['authorUsername'], posts['posts'][i]["content"]));
+            _postList.add(new Post(posts['posts'][i]['authorUsername'], posts['posts'][i]["content"], '',
+                posts['posts'][i]['groupName']));
           }
         }
       }
@@ -124,24 +134,53 @@ class _ProfileState extends State<Profile> {
       ),
     ));
     for (int i = 0; i < _postList.length; i++) {
-      widgetList.add(
-        new Container(
-          decoration: new BoxDecoration(
-            border: Border.all(color: globals.themeColor)
-          ),
-          child: new MaterialButton(
-            onPressed: () {
+      widgetList.add(new Container(
+        margin: new EdgeInsets.fromLTRB(12.0, 0.0, 12.0, 12.0),
+        decoration:
+        new BoxDecoration(border: Border.all(color: globals.themeColor)),
+        constraints: new BoxConstraints(maxWidth: 128.0, maxHeight: 168.0),
+        child: new MaterialButton(
+          padding: new EdgeInsets.all(0.0),
+          onPressed: () {
             Navigator.push(
-              context,
-              new MaterialPageRoute(
-                  builder: (BuildContext context) =>
-                      new ViewPost(_postList[i])));
-        },
-        child: new Text(_postList[i].content, softWrap: true,),
-            
-      )
-        ));
-      widgetList.add(new Padding(padding: new EdgeInsets.all(10.0)));
+                context,
+                new MaterialPageRoute(
+                    builder: (BuildContext context) =>
+                    new ViewPost(_postList[i])));
+          },
+          child: new Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  new Container(
+                    constraints: new BoxConstraints(maxWidth: 100.0, maxHeight: 32.0),
+                    padding: new EdgeInsets.all(6.0),
+                    margin: new EdgeInsets.fromLTRB(0.0, 0.0, 18.0, 4.0),
+                    child: new Text(_postList[i].authorUsername),
+                    decoration: new BoxDecoration(
+                      border: new Border.all(
+                          color: globals.themeColor, width: 2.0),
+                    ),
+                  ),
+                  new Container(
+                    constraints: new BoxConstraints(maxWidth: _textBoxWidth(_postList[i].hasTab), maxHeight: 196.0),
+                    margin: new EdgeInsets.fromLTRB(12.0, 2.0, 18.0, 6.0),
+                    padding: new EdgeInsets.all(4.0),
+                    child: new Text(_postList[i].content, maxLines: 6),
+                    decoration: new BoxDecoration(
+                      border: new Border.all(color: globals.themeColor),
+                    ),
+                  ),
+                ],
+              ),
+              new Text(_postList[i].tabRender()),
+            ],
+          ),
+        ),
+      ),
+      );
     }
     return widgetList;
   }
