b50dc599088bb6200e2de9c5307563b159ace4e3
Nick Steger <nsteger@iastate.edu>
2018-04-23 18:56:29 -0500

    Disalbe delete-user route

--- StringCenterNode/routes/api/user/deleteUser.js
+++ StringCenterNode/routes/api/user/deleteUser.js
@@ -14,19 +14,20 @@ function deleteUser(passport) {
 
     router.delete('/:username', passport.authenticate('jwt', { session: false }), function(req, res, next) {
         if (req.params.username === req.user.username || req.user.adminStatus === true) {
-            User.findOneAndRemove({ 'username': req.params.username }, function (error, user) {
-                if (error) {
-                    return res.json({ errors: [{ message: 'Something went wrong' }] }).status(500);
-                } else if (!user) {
-                    return res.json({ errors: [{ message: 'Username not found' }] }).status(500);
-                }
-                user.deleteUserInfo(function(err) {
-                    if (err) {
-                        return res.json({ errors: [{ message: 'Something went wrong' }] }).status(500);
-                    }
-                    res.json({ message: 'Successfully deleted user' }).status(200);
-                });
-            });
+            res.json({ message: 'Cannot delete user' });
+            // User.findOneAndRemove({ 'username': req.params.username }, function (error, user) {
+            //     if (error) {
+            //         return res.json({ errors: [{ message: 'Something went wrong' }] }).status(500);
+            //     } else if (!user) {
+            //         return res.json({ errors: [{ message: 'Username not found' }] }).status(500);
+            //     }
+            //     user.deleteUserInfo(function(err) {
+            //         if (err) {
+            //             return res.json({ errors: [{ message: 'Something went wrong' }] }).status(500);
+            //         }
+            //         res.json({ message: 'Successfully deleted user' }).status(200);
+            //     });
+            // });
         } else {
             res.json({ errors: [{ message: 'Unauthorized' }] });
         }
