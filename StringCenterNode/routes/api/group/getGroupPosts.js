b76451e5340e84122ae7369b98db5da2f01c0f4e
Nick Steger <nsteger@iastate.edu>
2018-04-23 19:48:17 -0500

    Change error message

--- StringCenterNode/routes/api/group/getGroupPosts.js
+++ StringCenterNode/routes/api/group/getGroupPosts.js
@@ -16,7 +16,7 @@ function getGroupPosts(passport){
         if(req.params.groupName){
             Post.find({"groupName" : req.params.groupName}, function(error, posts){
                 if (error) return res.json({ errors: [{ message: 'Something went wrong in finding a post' }] }).status(500);
-                if (posts.length == 0) return res.json("No posts");
+                if (posts.length == 0) return res.json({ errors: [{ message: 'No posts found' }] }).status(400);
                 Post.buildPostList(posts, function (error, ret) {
                     if (error) return res.json({errors: [{message: 'Something went wrong building the post list'}]}).status(500);
                     return res.json({posts : ret}).status(200);
