84040dbdc72758372442e0ee962064f411c478f4
mattb <mattbechtel123@gmail.com>
2018-05-07 22:36:04 -0500

    setting up to implement a home page

--- StringCenterNode/public/javascripts/home.js
+++ StringCenterNode/public/javascripts/home.js
@@ -11,3 +11,31 @@ var app = angular.module('home', ["ngRoute"]);
 app.config(['$httpProvider', function($httpProvider) {
     delete $httpProvider.defaults.headers.common["X-Requested-With"]
 }]);
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "/home",
+        controller : "homeCtrl"
+    })
+    .when("/tabs", {
+        templateUrl : "/tabs",
+        controller : "tabCtrl"
+    })
+    .when("/profile", {
+        templateUrl : "/profile",
+        controller : "profileCtrl"
+    })
+});
+
+app.controller('homeCtrl', function($scope){
+
+});
+
+app.controller('tabCtrl', function($scope){
+
+});
+
+app.controller('profileCtrl', function($scope){
+
+});
